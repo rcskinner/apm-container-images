99d1cb4a5a59bc14bb1ac616766841fdaf5bb983
rcskinner <ryan.skinner@datadoghq.com>
2024-08-14 13:47:47 -0600

    Updated the api to interface with a redis cache Removed a redundant route

--- python/main.py
+++ python/main.py
@@ -1,16 +1,37 @@
-from fastapi import FastAPI
+import json
 
-app = FastAPI() 
+from fastapi import FastAPI
+from redis import ConnectionPool, Redis
+from pydantic import BaseModel
+
+# Create the Redis Connection
+pool = ConnectionPool(host="localhost", port=6379, db=0)
+r = Redis(connection_pool=pool)
+
+
+app = FastAPI()
 
 
 @app.get("/")
-def read_root(): 
-    return {"Hello":"World"}
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/hello")
-def return_hello(): 
-    return "Hello, world!"
 
-@app.get("/items/{item_id}") 
-def read_item(item_id):
-    return {"id":item_id}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float
+
+
+@app.post("/create_item")
+async def create_item(item: Item):
+    r.set(item.name, item.model_dump_json())
+    return item
+
+
+@app.get("/retrieve_item/{item_name}")
+async def retrieve_item(item_name):
+    item = r.get(item_name)
+    item = json.loads(item)
+    return item
